cb01231c2f1ea5401e54e4024dd0ad4244d1e7ff
CarolinaSolamente <bethany@rambler.ru>
2016-02-03 13:24:41 +0300

    Модификация и удаление контактов (улучшение)

--- test/test_modify_user.py
+++ test/test_modify_user.py
@@ -2,5 +2,5 @@ from model.user import User
 
 def test_modify_fist_user(app):
     app.session.login(username="admin", password="secret")
-    app.user.modify_first_user(User(fname="mod102", mname="m2", lname="m3", nname="m4", title="m5", company="m6", address="m7", home="m8", mobile="m9", twork="m10", fax="m11", email="m1.32.@6.ru", email2="m12", email3="m13", homepage="m14", byear="2100", ayear="2200", address2="m15", phone2="m16", notes="m17"))
+    app.user.modify_first_user(User(fname="mod102", mname="m2", lname="m3", nname="m4", title="m5", company="m6", address="m7", home="m8", mobile="m9", twork="m10", fax="m11", email="m1.32.@6.ru", email2="m12", email3="m13", homepage="m14", byear="2100", ayear="2200", address2="m15", phone2="m16", notes="mod17"))
     app.session.logout()
