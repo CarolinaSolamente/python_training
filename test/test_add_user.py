08dd0c7540becd2c4397b30052bb94b83338cf13
CarolinaSolamente <bethany@rambler.ru>
2016-02-08 17:08:26 +0300

    Интеллектуальная фикстура

--- test/test_add_user.py
+++ test/test_add_user.py
@@ -2,6 +2,5 @@
 from model.user import User
 
 def test_add_user(app):
-    app.session.login(username="admin", password="secret")
     app.user.create(User(fname="102", mname="2", lname="3", nname="4", title="5", company="6", address="7", home="8", mobile="9", twork="10", fax="11", email="1.32.@6.ru", email2="12", email3="13", homepage="14", byear="2000", ayear="2100", address2="15", phone2="16", notes="17"))
-    app.session.logout()
+
