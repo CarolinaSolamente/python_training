18e2d911d256e9f47c957dd35beca30adb2b8665
CarolinaSolamente <bethany@rambler.ru>
2016-03-11 19:35:02 +0300

    случайность в тестах

--- fixture/group_old.py
+++ fixture/group_old.py
@@ -34,12 +34,9 @@ class GroupHelper:
             wd.find_element_by_name(field_name).send_keys(text)
 
     def delete_first_group(self):
-            self.delete_group_by_index(0)
-
-    def delete_group_by_index(self, index):
              wd = self.app.wd
              self.open_groups_page()
-             self.select_group_by_index(index)
+             self.select_first_group()
             # submit deletion
              wd.find_element_by_name("delete").click()
              self.return_to_groups_page()
@@ -49,14 +46,10 @@ class GroupHelper:
         wd = self.app.wd
         wd.find_element_by_name("selected[]").click()
 
-    def select_group_by_index(self, index):
-        wd = self.app.wd
-        wd.find_elements_by_name("selected[]")[index].click()
-
-    def modify_group_by_index(self, index, new_group_data):
+    def modify_first_group(self, new_group_data):
                 wd = self.app.wd
                 self.open_groups_page()
-                self.select_group_by_index(index)
+                self.select_first_group()
             # open modification form
                 wd.find_element_by_name("edit").click()
             # fill group form
@@ -77,9 +70,6 @@ class GroupHelper:
                 wd.find_element_by_name("update").click()
                 self.return_to_groups_page()
                 self.group_cache = None
-
-    def modify_first_group(self):
-            self.modify_group_by_index(0)
 
     def open_groups_page(self):
                 wd = self.app.wd
--- test/test_modify_group_old.py
+++ test/test_modify_group_old.py
@@ -1,18 +1,14 @@
 
 from model.group import Group
-from random import randrange
 
 def test_modify_group_name(app):
-    if app.group.count() == 0:
-            app.group.create(Group(name="add_new-gr"))
     old_groups = app.group.get_group_list()
-    index = randrange(len(old_groups)) # random number 0 until old_groups
     group = Group(name="New group")
-    group.id = old_groups[index].id
-    app.group.modify_group_by_index(index, group)
+    group.id = old_groups[0].id
+    app.group.modify_first_group(Group(name="New group"))
     new_groups = app.group.get_group_list()
     assert len(old_groups) == len(new_groups)
-    old_groups[index] = group
+    old_groups[0] = group
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 
