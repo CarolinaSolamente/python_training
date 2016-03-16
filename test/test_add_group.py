bb9002684270e9ae8c07248fc33a69dd330a494f
CarolinaSolamente <bethany@rambler.ru>
2016-03-16 17:48:59 +0300

    генерация тестовых данных и загрузка тестовых данных из файла - JSON

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,20 +1,14 @@
 # -*- coding: utf-8 -*-
 
 from model.group import Group
-import pytest
-from data.add_group import constant as testdata
 
 
-# тестовые данные будут передовать в качестве параметра (через group)
-# кто их туда будет передовать, кончно тестовый фреймворк
 
 
-@pytest.mark.parametrize("group", testdata, ids=[repr(x) for x in testdata])
-def test_add_group(app, group):
+def test_add_group(app, json_groups):
+    group = json_groups
     old_groups = app.group.get_group_list()
-    #    group = Group(name="testN", header="fffdf", footer="test")
     app.group.create(group)
-    #   app.session.logout()
     assert len(old_groups) + 1 == app.group.count()
     new_groups = app.group.get_group_list()
     old_groups.append(group)
