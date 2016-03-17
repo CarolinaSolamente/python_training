c952406e37e91c160c96a0266448ef0fecf46db2
CarolinaSolamente <bethany@rambler.ru>
2016-03-17 17:05:31 +0300

    проверяем данные, загруженные из БД

--- fixture/db.py
+++ fixture/db.py
@@ -9,6 +9,7 @@ class DbFixture:
         self.user = user
         self.password = password
         self.connection = mysql.connector.connect(host=host, database=name, user=user, password=password)
+        self.connection.autocommit = True # сбрасываем кеш в БД
 
     def get_group_list(self):
         list = [] # инициализируем список
--- fixture/group.py
+++ fixture/group.py
@@ -36,6 +36,15 @@ class GroupHelper:
     def delete_first_group(self):
             self.delete_group_by_index(0)
 
+    def delete_group_by_id(self, id):
+             wd = self.app.wd
+             self.open_groups_page()
+             self.select_group_by_id(id)
+            # submit deletion
+             wd.find_element_by_name("delete").click()
+             self.return_to_groups_page()
+             self.group_cache = None
+
     def delete_group_by_index(self, index):
              wd = self.app.wd
              self.open_groups_page()
@@ -52,6 +61,10 @@ class GroupHelper:
     def select_group_by_index(self, index):
         wd = self.app.wd
         wd.find_elements_by_name("selected[]")[index].click()
+
+    def select_group_by_id(self, id):
+        wd = self.app.wd
+        wd.find_element_by_css_selector("input[value='%s']" % id).click()
 
     def modify_group_by_index(self, index, new_group_data):
                 wd = self.app.wd
