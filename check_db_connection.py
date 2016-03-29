63b74a03a958fea631b9f0c35bd1afe3e9a8a968
CarolinaSolamente <bethany@rambler.ru>
2016-03-29 13:36:53 +0300

    Добавили requirements.txt

--- check_db_connection.py
+++ check_db_connection.py
@@ -1,14 +1,11 @@
 import mysql.connector
-from fixture.orm import ORMFixture
-from model.group import Group
 
-
-db = ORMFixture(host='127.0.0.1', name="addressbook", user="root", password="")
+connection = mysql.connector.connect(host='127.0.0.1', database="addressbook", user="root", password="")
 
 try:
-    l = db.get_contacts_not_in_group(Group(id="11"))
-    for item in l:
-        print(item)
-    print(len(l))
+    cursor = connection.cursor()
+    cursor.execute("select * from addressbook")
+    for row in cursor.fetchall():
+        print(row)
 finally:
-    pass # db.destroy()
+    connection.close()
