c3402b8da9622d27d55561d4f5f5115d90776ea4
CarolinaSolamente <bethany@rambler.ru>
2016-03-30 17:21:38 +0300

    bdd_ci

--- bdd/group_scenarios.py
+++ bdd/group_scenarios.py
@@ -8,6 +8,6 @@ def test_add_new_group():
     pass
 
 
-@scenario('groups.feature', 'Delete a group')
-def test_delete_group():
-    pass
+# @scenario('groups.feature', 'Delete a group')
+# def test_delete_group():
+#     pass
